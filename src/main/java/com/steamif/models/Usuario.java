6287279a8d94cf254e0fa7a3845d3fa5c02e263e
Felipkw <bfelipedelima@gmail.com>
2022-12-28 13:36:52 +0000

    Reorganização do código

--- src/main/java/com/steamif/models/Usuario.java
+++ src/main/java/com/steamif/models/Usuario.java
@@ -6,34 +6,25 @@ public class Usuario {
 
     // Atributos
     private int id;
-    private double saldo;
-    private String dataNascimento;
     private String email;
-    private String nomeUsuario;
+    private String nome;
     private String senha;
     private boolean adm;
     private ArrayList<Jogo> biblioteca;
 
     // Construtores
-    public Usuario(int id, String nomeUsuario, String email, String senha, String dataNascimento, double saldo, 
-    boolean adm, ArrayList<Jogo> biblioteca) {
-        this.id = id;
-        this.nomeUsuario = nomeUsuario;
+    public Usuario(String nome, String email, String senha, boolean adm) {
+        this.nome = nome;
         this.email = email;
         this.senha = senha;
-        this.dataNascimento = dataNascimento;
-        this.saldo = saldo;
-        this.biblioteca = biblioteca;
         this.adm = adm;
     }
 
-    public Usuario(String nomeUsuario, String email, String senha, String dataNascimento, double saldo, 
-    boolean adm, ArrayList<Jogo> biblioteca) {
-        this.nomeUsuario = nomeUsuario;
+    public Usuario(int id, String nome, String email, String senha, boolean adm, ArrayList<Jogo> biblioteca) {
+        this.id = id;
+        this.nome = nome;
         this.email = email;
         this.senha = senha;
-        this.dataNascimento = dataNascimento;
-        this.saldo = saldo;
         this.biblioteca = biblioteca;
         this.adm = adm;
     }
@@ -47,12 +38,12 @@ public class Usuario {
         this.id = id;
     }
 
-    public String getNomeUsuario() {
-        return nomeUsuario;
+    public String getNome() {
+        return nome;
     }
 
-    public void setNomeUsuario(String nomeUsuario) {
-        this.nomeUsuario = nomeUsuario;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String getEmail() {
@@ -69,22 +60,6 @@ public class Usuario {
 
     public void setSenha(String senha) {
         this.senha = senha;
-    }
-
-    public String getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(String dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
     }
 
     public ArrayList<Jogo> getBiblioteca() {
