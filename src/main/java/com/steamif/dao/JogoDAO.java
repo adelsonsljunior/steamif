c88a241ce3a3eefbdab18dff6e7aef38ad33db2f
adelsonsljuinor <junioradelsonst@gmail.com>
2022-12-30 06:17:55 -0300

    comentarios no dao

--- src/main/java/com/steamif/dao/JogoDAO.java
+++ src/main/java/com/steamif/dao/JogoDAO.java
@@ -10,23 +10,26 @@ import java.util.ArrayList;
 import com.steamif.conexao.Conexao;
 import com.steamif.models.Jogo;
 
+public class JogoDAO {
 
-public class JogoDAO{
-            
-    public void cadastrar(Jogo jogo){
+    public void cadastrar(Jogo jogo) {
 
+        // Conexão com o banco de dados
         Connection con = null;
         PreparedStatement pstm = null;
+        // String com o comando SQL para inserir os dados de jogos
         String sql = "INSERT INTO jogos (nome, preco, desconto, desenvolvedora, distribuidora, classificacao) VALUES (?, ?, ?, ?, ?, ?)";
 
         try {
             con = Conexao.getConnection();
             pstm = con.prepareStatement(sql);
+            // Setar os valores do comando SQL
             pstm.setString(1, jogo.getNome());
             pstm.setDouble(3, jogo.getPreco());
             pstm.setDouble(4, jogo.getDesconto());
             pstm.setString(5, jogo.getDesenvolvedora());
             pstm.setString(6, jogo.getDistribuidora());
+            // Executar o comando SQL
             pstm.execute();
 
         } catch (Exception e) {
@@ -48,66 +51,81 @@ public class JogoDAO{
         }
     }
 
-    public ArrayList<Jogo> listarJogos(){
-            
-            Connection con = null;
-            PreparedStatement pstm = null;
-            ResultSet rs = null;
-            ArrayList<Jogo> jogos = new ArrayList<Jogo>();
-    
-            String sql = "SELECT * FROM jogos";
-    
-            try {
-                con = Conexao.getConnection();
-                pstm = con.prepareStatement(sql);
-                rs = pstm.executeQuery();
-    
-                while (rs.next()) {
-                    
-                    int id = rs.getInt("id");
-                    String nome = rs.getString("nome");
-                    String descricao = rs.getString("descricao");
-                    double preco = rs.getDouble("preco");
-                    double desconto = rs.getDouble("desconto");
-                    String desenvolvedora = rs.getString("desenvolvedora");
-                    String distribuidora = rs.getString("distribuidora");
-                    String genero = rs.getString("genero");
-                    int classificacao = rs.getInt("classificacao");
-                    
-                    Jogo jogo = new Jogo(id, desconto, preco, nome, desenvolvedora, distribuidora, genero, classificacao);
-                    jogos.add(jogo);
-                }
-    
-            } catch (Exception e) {
-                e.printStackTrace();
+    public ArrayList<Jogo> listarJogos() {
 
-            } finally {
-                    
-                    // Fechar as conexões
-                    try {
-                        if (pstm != null) {
-                            pstm.close();
-                        }
-    
-                        if (con != null) {
-                            con.close();
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
+        // Conexão com o banco de dados
 
-            return jogos;
-    }
-
-    public void atualizarJogo(Jogo jogo){
         Connection con = null;
         PreparedStatement pstm = null;
-        String sql = "UPDATE jogos SET nome = ?, nome = ?, preco = ?, desconto = ?, desenvolvedora = ?, distribuidora = ?, classificacao = ? WHERE id = ?";
+        ResultSet rs = null;
+        ArrayList<Jogo> jogos = new ArrayList<Jogo>();
+
+        // String com o comando SQL para consultar todos os jogos
+
+        String sql = "SELECT * FROM jogos";
 
         try {
             con = Conexao.getConnection();
             pstm = con.prepareStatement(sql);
+            // Executar o comando SQL
+            rs = pstm.executeQuery();
+
+            // Enquanto houver registros, adicionar na lista
+            while (rs.next()) {
+
+                // Criar um objeto do tipo Jogo
+
+                int id = rs.getInt("id");
+                String nome = rs.getString("nome");
+                double preco = rs.getDouble("preco");
+                double desconto = rs.getDouble("desconto");
+                String desenvolvedora = rs.getString("desenvolvedora");
+                String distribuidora = rs.getString("distribuidora");
+                String genero = rs.getString("genero");
+                int classificacao = rs.getInt("classificacao");
+
+                Jogo jogo = new Jogo(id, desconto, preco, nome, desenvolvedora, distribuidora, genero, classificacao);
+                jogos.add(jogo);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } finally {
+
+            // Fechar as conexões
+            try {
+                if (pstm != null) {
+                    pstm.close();
+                }
+
+                if (con != null) {
+                    con.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Retornar a lista de jogos
+
+        return jogos;
+    }
+
+    public void atualizarJogo(Jogo jogo) {
+
+        Connection con = null;
+        PreparedStatement pstm = null;
+        // String com o comando SQL para atualizar os dados de jogos
+        String sql = "UPDATE jogos SET nome = ?, nome = ?, preco = ?, desconto = ?, desenvolvedora = ?, distribuidora = ?, classificacao = ? WHERE id = ?";
+
+        try {
+
+            // Conexão com o banco de dados
+            con = Conexao.getConnection();
+            pstm = con.prepareStatement(sql);
+
+            // Setar os valores do comando SQL
             pstm.setString(1, jogo.getNome());
             pstm.setString(2, jogo.getNome());
             pstm.setDouble(3, jogo.getPreco());
@@ -116,6 +134,7 @@ public class JogoDAO{
             pstm.setString(6, jogo.getDistribuidora());
             pstm.setInt(7, jogo.getClassificacao());
             pstm.setInt(8, jogo.getId());
+            // Executar o comando SQL
             pstm.execute();
 
         } catch (Exception e) {
@@ -137,15 +156,18 @@ public class JogoDAO{
         }
     }
 
-    public void excluirJogo(int id){
+    public void excluirJogo(int id) {
         Connection con = null;
         PreparedStatement pstm = null;
+        // String com o comando SQL para excluir os dados de jogos por id
         String sql = "DELETE FROM jogos WHERE id = ?";
 
         try {
             con = Conexao.getConnection();
             pstm = con.prepareStatement(sql);
+            // Setar os valores do comando SQL
             pstm.setInt(1, id);
+            // Executar o comando SQL
             pstm.execute();
 
         } catch (Exception e) {
@@ -167,17 +189,20 @@ public class JogoDAO{
         }
     }
 
-    public void comprar(int idUsuario, int idJogo){
+    public void comprar(int idUsuario, int idJogo) {
 
         Connection con = null;
         PreparedStatement pstm = null;
+        // String com o comando SQL para conexao de jogos e usuarios
         String sql = "INSERT INTO jogos_usuarios (id_usuario, id_jogo) VALUES (?, ?)";
 
         try {
             con = Conexao.getConnection();
             pstm = con.prepareStatement(sql);
+            // Setar os valores do comando SQL
             pstm.setInt(1, idUsuario);
             pstm.setInt(2, idJogo);
+            // Executar o comando SQL
             pstm.execute();
 
         } catch (Exception e) {
@@ -197,9 +222,7 @@ public class JogoDAO{
                 e.printStackTrace();
             }
         }
-        
+
     }
 
-
-    
 }
--- src/main/java/com/steamif/dao/UsuarioDAO.java
+++ src/main/java/com/steamif/dao/UsuarioDAO.java
@@ -13,9 +13,15 @@ import com.steamif.models.Jogo;
 
 public class UsuarioDAO {
 
+    // Atributo para armazenar o id do usuário logado
     private int id;
     
     public boolean login(String email, String senha) {
+
+        /* Método de Login - São passados o email e a senha como parametros para verificar se há
+         * um usuário com esses dados no banco de dados. Caso haja, o método retorna true e
+         * o usuário é logado. Caso contrário, retorna false e o usuário não é logado.
+         */
 
         boolean logado = false;
         Usuario usuario = null;
@@ -23,22 +29,28 @@ public class UsuarioDAO {
         Connection con = null;
         PreparedStatement pstm = null;
         ResultSet rs = null;
+
+        // String com o comando SQL buscar um usuário por email e senha
         String sql = "SELECT * FROM usuarios WHERE email = ? AND senha = ?";
 
         try {
             con = Conexao.getConnection();
             pstm = con.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            // Setar os valores do comando SQL
             pstm.setString(1, email);
             pstm.setString(2, senha);
+            // Executar o comando SQL e pegar os resultados da consulta
             rs = pstm.executeQuery();
-
+            // Como é esperado apenas um resultado, o cursor é movido para o primeiro registro
             rs.first();
+            // Pegar os dados do usuário
             int id = rs.getInt("id");
             this.id = id;
             String nome = rs.getString("nome");
             String email2 = rs.getString("email");
             String senha2 = rs.getString("senha");
             boolean administrador = rs.getBoolean("administrador");
+            // Criar um objeto do tipo Usuario com os dados pegos do banco de dados
             usuario = new Usuario(id, nome, email2, senha2, administrador, new ArrayList<>());
 
         } catch (Exception e) {
@@ -63,10 +75,12 @@ public class UsuarioDAO {
             }
         }
 
+        // Se o usuário não for nulo, significa que ele foi encontrado no banco de dados
         if (usuario != null) {
             logado = true;
         }
 
+        // Retornar o valor da variável logado
         return logado;
     }
 
@@ -77,20 +91,26 @@ public class UsuarioDAO {
         Connection con = null;
         PreparedStatement pstm = null;
         ResultSet rs = null;
+
+        // String com o comando SQL buscar um usuário por id
         String sql = "SELECT * FROM usuarios WHERE id = ?";
 
         try {
             con = Conexao.getConnection();
             pstm = con.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            // Setar os valores do comando SQL
             pstm.setInt(1, id);
+            // Executar o comando SQL e pegar os resultados da consulta
             rs = pstm.executeQuery();
-
+            // Como é esperado apenas um resultado, o cursor é movido para o primeiro registro
             rs.first();
+            // Pegar os dados do usuário
             int id2 = rs.getInt("id");
             String nome = rs.getString("nome");
             String email = rs.getString("email");
             String senha = rs.getString("senha");
             boolean administrador = rs.getBoolean("administrador");
+            // Criar um objeto do tipo Usuario com os dados pegos do banco de dados
             usuario = new Usuario(id2, nome, email, senha, administrador, new ArrayList<>());
 
         } catch (Exception e) {
@@ -115,7 +135,7 @@ public class UsuarioDAO {
                 e.printStackTrace();
             }
         }
-
+        // Retornar o usuário
         return usuario;
     }
 
@@ -123,14 +143,17 @@ public class UsuarioDAO {
 
         Connection con = null;
         PreparedStatement pstm = null;
+        // String com o comando SQL para cadastrar um usuário
         String sql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
 
         try {
             con = Conexao.getConnection();
             pstm = con.prepareStatement(sql);
+            // Setar os valores do comando SQL
             pstm.setString(1, usuario.getNome());
             pstm.setString(2, usuario.getEmail());
             pstm.setString(3, usuario.getSenha());
+            // Executar o comando SQL
             pstm.execute();
 
             System.out.println("Usuário cadastrado com sucesso");
@@ -155,26 +178,42 @@ public class UsuarioDAO {
 
     public ArrayList<Jogo> listarJogosDoUsuario(int idUsuario) {
 
+        /*Só brinque. Agora brinque. Esse método ficou muito bom */
+
+        /*Esse meétodo tem dois comandos sql, o primeiro retorna o id dos  
+         * jogos de um usuario e o segundo retorna os dados do jogo com base no id
+         */
+
         Connection con = null;
         PreparedStatement pstm = null;
         ResultSet rs = null;
+        /*1 - Comando que SQL que retorna os ids dos jogos de um usuario */
         String sql = "SELECT id_jogo FROM jogos_usuarios WHERE id_usuario = ?";
+        /*2 - Comando que o resultado do camando anterior */
         String sql2 = "SELECT * FROM jogos WHERE id = ?";
         ArrayList<Jogo> jogos = new ArrayList<Jogo>();
 
         try {
             con = Conexao.getConnection();
             pstm = con.prepareStatement(sql);
+            // Setar os valores do primerio comando SQL
             pstm.setInt(1, idUsuario);
+            //Executar o comando SQL e pegar os resultados da consulta
             rs = pstm.executeQuery();
 
+            // Enquanto houver jogos para o usuário no resultado da consulta
             while (rs.next()) {
 
                 int idJogo = rs.getInt("id_jogo");
                 pstm = con.prepareStatement(sql2, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                // Setar os valores do segundo comando SQL
                 pstm.setInt(1, idJogo);
+                // Executar o primeiro comando SQL e pegar os resultados da consulta
                 ResultSet rs2 = pstm.executeQuery();
+
+                // Como é esperado apenas um resultado, o cursor é movido para o primeiro registro
                 rs2.first();
+                // Pegar os dados do jogo
                 int id = rs2.getInt("id");
                 String nome = rs2.getString("nome");
                 String genero = rs2.getString("genero");
@@ -183,7 +222,7 @@ public class UsuarioDAO {
                 double preco = rs2.getDouble("preco");
                 double desconto = rs2.getDouble("desconto");
                 int classificacao = rs2.getInt("classificacao");
-
+                // Criar um objeto do tipo Jogo com os dados pegos do banco de dados
                 Jogo jogo = new Jogo(id, preco, desconto, nome, desenvolvedora, distribuidora, genero, classificacao);
                 jogos.add(jogo);
             }
@@ -210,7 +249,7 @@ public class UsuarioDAO {
                 e.printStackTrace();
             }
         }
-
+        // Retornar jogos do usuário
         return jogos;
     }
 
@@ -219,22 +258,26 @@ public class UsuarioDAO {
         Connection con = null;
         PreparedStatement pstm = null;
         ResultSet rs = null;
+        // String com o comando SQL para listar todos os usuários
         String sql = "SELECT * FROM usuarios";
         ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
 
         try {
             con = Conexao.getConnection();
             pstm = con.prepareStatement(sql);
+            // Executar o comando SQL e pegar os resultados da consulta
             rs = pstm.executeQuery();
 
+            // Enquanto houver usuários no banco de dados
             while (rs.next()) {
-
+                // Pegar os dados do usuário
                 int id = rs.getInt("id");
                 String nome = rs.getString("nome");
                 String email = rs.getString("email");
                 String senha = rs.getString("senha");
                 boolean adm = rs.getBoolean("administrador");
                 ArrayList<Jogo> biblioteca = listarJogosDoUsuario(id);
+                // Criar um objeto usuário
                 Usuario usuario = new Usuario(id, nome, email, senha, adm, biblioteca);
                 usuarios.add(usuario);
             }
@@ -268,14 +311,17 @@ public class UsuarioDAO {
 
         Connection con = null;
         PreparedStatement pstm = null;
+        /// String com o comando SQL para atualizar um usuário
         String sql = "UPDATE usuarios SET nome = ?, email = ? WHERE id = ?";
 
         try {
             con = Conexao.getConnection();
             pstm = con.prepareStatement(sql);
+            // Passar os parâmetros para o comando SQL
             pstm.setString(1, nome);
             pstm.setString(2, email);
             pstm.setInt(3, id);
+            // Executar o comando SQL
             pstm.execute();
 
             System.out.println("Usuário alterado com sucesso");
@@ -303,19 +349,26 @@ public class UsuarioDAO {
         Connection con = null;
         
         PreparedStatement pstm = null;
+        // String com o comando SQL para os jogos de um usuario, assim não vai dar erro de chave estrangeira
         String sql = "DELETE FROM jogos_usuarios WHERE id_usuario = ?";
 
         PreparedStatement pstm2 = null;
+        /*String com o comando SQL para excluir um usuário
+        */
         String sql2 = "DELETE FROM usuarios WHERE id = ?";
 
         try {
             con = Conexao.getConnection();
             pstm = con.prepareStatement(sql);
+            // Setar os valores para o comando SQL
             pstm.setInt(1, id);
+            // Executar o primeiro comando SQL
             pstm.execute();
 
             pstm2 = con.prepareStatement(sql2);
+            // Setar os valores para o segundo comando SQL
             pstm2.setInt(1, id);
+            // Executar o segundo comando SQL
             pstm2.execute();
 
             System.out.println("Usuário excluído com sucesso");
@@ -338,6 +391,7 @@ public class UsuarioDAO {
         }
     }
 
+    // Métodos para o id do usuário
     public int getId() {
         return id;
     }
